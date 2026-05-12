f4a91beb8b9fc3514e49987ea4880ae8427410e5
Mertcan Boztoprak <mertcan.boztoprak@hotmail.com>
2026-05-12 09:40:19 +0300

    Hello World

---
+++ MAB_Spark/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MAB_Spark
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
